af9bf0cbaa7d178b5e858ec9214f4017cc5f26be
JosephFed <S10241860@connect.np.edu.sg>
2024-11-12 13:02:56 +0800

    revert test3,2

--- index.js
+++ index.js
@@ -1,58 +1,22 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const app = express();
-// const cors = require('cors');
-// const port = 3000;
-
-// // Middleware to parse incoming JSON
-// app.use(bodyParser.json());
-// app.use(cors());
-
-// // Route to receive NFC data
-// app.post('/receive-nfc-data', (req, res) => {
-//     const nfcData = req.body.data; // The NFC data sent from the phone
-//     console.log('Received NFC data:', nfcData);
-
-//     // Respond back
-//     res.json({ status: 'success', message: 'NFC data received' });
-// });
-
-// app.listen(port, () => {
-//     console.log(`Server running at http://localhost:${port}`);
-// });
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
 const port = 3000;
-const path = require('path');
 
 // Middleware to parse incoming JSON
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'docs')));
-
-// Variable to store NFC data
-let nfcData = '';
-
-// Route to receive NFC data from the Android phone
+// Route to receive NFC data
 app.post('/receive-nfc-data', (req, res) => {
-    nfcData = req.body.data; // Save the NFC data sent from the phone
+    const nfcData = req.body.data; // The NFC data sent from the phone
     console.log('Received NFC data:', nfcData);
+
+    // Respond back
     res.json({ status: 'success', message: 'NFC data received' });
 });
 
-// Route to fetch NFC data for the laptop's HTML page
-app.get('/nfc-data', (req, res) => {
-    res.json({ data: nfcData });
-});
-
-// Serve static files (HTML, CSS, JS)
-app.use(express.static('docs'));
-
-// Start the server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
